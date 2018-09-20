6e3aee77c72d5980e2a053a9b26ff685c83268ed
Maely Gauthier <mg3342@raijin3.(none)>
2018-09-20 16:07:32 +1000

    clean script up

--- pathseq_scoring_pbs.sh
+++ pathseq_scoring_pbs.sh
@@ -1,36 +1,17 @@
 #!/bin/bash
-
 ### Job name
-#PBS -N pathseq_testing
-
-### Join queuing system output and error files into a single output file
+#PBS -N pathseq_scoring
 #PBS -j oe
-
-### Send email to user when job ends or aborts
 #PBS -m ae
-
-### email address for user
 #PBS -M m.gauthier@garvan.org.au
-
-### Queue name that job is submitted to
 #PBS -q normal
-
-### Provide project to be billed
 #PBS -P jp48
-
-### Make the data readable to everyone in the group
 #PBS -W umask=027
-
-### Set out (Use job ID so it's unique)
 #PBS -e /g/data3/ba08/pathseq/pathseq_tutorial.${ERRFILE}_${TASK_ID}
-
-### Request nodes, memory, walltime. NB THESE ARE REQUIRED.
-##### run, change these entries
 #PBS -l ncpus=1
 #PBS -l mem=8Gb
 #PBS -l walltime=1:00:00
 
-# Load required modules
 module load java
 module load gatk/4.0
 module load BWA/0.7.17
@@ -41,24 +22,12 @@ module load samtools
 echo Running on host `hostname`
 echo Time is `date`
 
-# This jobs working directory
 echo Working directory is ${PBS_O_WORKDIR}
 cd ${PBS_O_WORKDIR}
-SAMPLE=184577_T
 
-
-#samtools index ${SAMPLE}_bwa_paired.bam
-#samtools index ${SAMPLE}_unpaired.bam
-
-#gatk PathSeqScoreSpark  \
-#   --paired-input /g/data3/ba08/pathseq/${SAMPLE}_bwa_paired.bam \
-#   --unpaired-input /g/data3/ba08/pathseq/${SAMPLE}_unpaired.bam \
-#   --taxonomy-file /g/data3/ba08/pathseq/pathseq_taxonomy.db \
-#   --scores-output /g/data3/ba08/pathseq/scores.txt \
-#   --output /g/data3/ba08/pathseq/output_reads.bam \
-#   --min-score-identity 0.90 \
-#   --identity-margin 0.02
-
+#Either specify name in script or on command line 
+#qsub -v SAMPLE=TCGA_CV_7255_N pathseq_scoring_pbs.sh 
+#SAMPLE=184577_T
 
 gatk --java-options "-Xmx8g" PathSeqScoreSpark  \
      --paired-input ${SAMPLE}_bwa_paired.bam \
